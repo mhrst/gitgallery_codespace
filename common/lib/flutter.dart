82c78f91d80d86bf3a5d2db9261628ec8c8a00ee
Matthew Hurst <matt@workpail.com>
2024-03-03 09:26:02 -0500

    Add empty page transition

--- common/lib/flutter.dart
+++ common/lib/flutter.dart
@@ -5,6 +5,7 @@ export 'src/flutter/app.dart';
 export 'src/flutter/colors.dart';
 export 'src/flutter/context.dart';
 export 'src/flutter/hack_scroll_controller.dart';
+export 'src/flutter/page_transitions.dart';
 export 'src/flutter/platform.dart';
 export 'src/flutter/router.dart';
 export 'src/flutter/sign_in_with_apple_button.dart';
